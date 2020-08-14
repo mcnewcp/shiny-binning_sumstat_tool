3fc3d1a742306274eb095a53f9262791bfb7370a
Coy McNew <mcnewcp@gmail.com>
2020-08-14 12:54:43 -0500

    modified method to include a specified start date and time

--- sumstat.R
+++ sumstat.R
@@ -6,8 +6,8 @@
 file_name <- "example_data.xlsx"  #file selection in shiny
 sheet_name <- "Another Data Sheet" #drop down selection in shiny
 bin_min <- 15 #bin width in minutes, integer entry in shiny
-date_time_col <- "date_time" #drop down selection in shiny, populated from xlsx
-value_col <- "Value" #drop down selection in shiny, populated from xlsx
+date_time_col <- "different_date_time_col" #drop down selection in shiny, populated from xlsx
+value_col <- "Value_2" #drop down selection in shiny, populated from xlsx
 
 ###read file
 dataDF <- read.xlsx(file_name, sheet_name)
@@ -16,10 +16,21 @@ dataDF <- read.xlsx(file_name, sheet_name)
 date_time_col <- sym(date_time_col)
 value_col <- sym(value_col)
 
+#select min date time rounded down to 15 minutes
+start_date_time_selection <- dataDF %>%
+  pull(!!date_time_col) %>%
+  min() %>% convertToDateTime() %>%
+  floor_date("15 minutes")
+
 statDF <- dataDF %>%
   select(!!date_time_col, !!value_col) %>%
   #fix date time
-  mutate_at(vars(date_time_col), convertToDateTime) %>%
+  mutate_at(vars(!!date_time_col), convertToDateTime) %>%
+  #add na row at start_date_time_selection for binning
+  bind_rows(
+    tibble(!!date_time_col := start_date_time_selection, !!value_col := NA)
+  ) %>% 
+  arrange(!!date_time_col) %>%
   #bin data
   mutate(start_time = cut(!!date_time_col, breaks = paste(bin_min, "mins"))) %>%
   #convert to date time
@@ -27,7 +38,7 @@ statDF <- dataDF %>%
   mutate(end_time = start_time + minutes(bin_min)) %>%
   #generate stats grouped by bin start time
   group_by(start_time) %>%
-  mutate(max_value = max(!!value_col), min_value = min(!!value_col)) %>%
+  mutate(max_value = max(!!value_col, na.rm = TRUE), min_value = min(!!value_col, na.rm = TRUE)) %>%
   ungroup() %>%
   #label rows as max or min
   mutate(
