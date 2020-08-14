1a1ebaa6fd2f5274e0d017021adc81206a1bd9bf
Coy McNew <mcnewcp@gmail.com>
2020-08-14 12:54:24 -0500

    extended example dataset length

--- example_data.R
+++ example_data.R
@@ -7,12 +7,12 @@ p_load(tidyverse, lubridate, openxlsx)
 write.xlsx(
   list(
     "Data Sheet 1" = tibble(
-      date_time = seq.POSIXt(ymd_hms("2020-08-09 10:13:50"), length.out=2000, by="10 sec"),
-      Value = rlnorm(2000, 4)
+      date_time = seq.POSIXt(ymd_hms("2020-08-09 10:13:50"), length.out=5000, by="10 sec"),
+      Value = rlnorm(5000, 4)
     ),
     "Another Data Sheet" = tibble(
-      different_date_time_col = seq.POSIXt(ymd_hms("2020-08-09 10:13:50"), length.out=2000, by="10 sec"),
-      Value_2 = rlnorm(2000, 4)
+      different_date_time_col = seq.POSIXt(ymd_hms("2020-08-09 10:13:50"), length.out=5000, by="10 sec"),
+      Value_2 = rlnorm(5000, 4)
     )
   ),
   "example_data.xlsx"
